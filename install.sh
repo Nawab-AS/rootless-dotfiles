3e65837ce996f71e72bb1ef579b7b292c4c638ee
Hamid Syed <59747708+Nawab-AS@users.noreply.github.com>
2026-09-17 18:31:33 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cd ~
+
+# download fish binary
+wget https://github.com/fish-shell/fish-shell/releases/download/4.9.3/fish-4.9.3-linux-x86_64.tar.xz
+
+# extract
+tar -xJf ~/fish-4.9.3-linux-x86_64.tar.xz
+rm ~/fish-4.9.3-linux-x86_64.tar.xz
+
+# move to local bin
+mkdir -p ~/.local/bin
+mv ~/fish ~/.local/bin/fish
+
+# write pseudo-default shell
+cat << 'EOF' > ~/.bashrc
+export PATH="$HOME/.local/bin:$PATH"
+export TERM=xterm-256color
+
+if [[ $- == *i* ]]; then
+    if [ "$SHLVL" -eq 1 ] && [ -x "$(command -v fish)" ]; then
+        export SHELL=$(command -v fish)
+        exec fish
+    fi
+fi
+EOF
+
+cat << 'EOF' > ~/.profile
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+EOF
+
+
+# install nvim
+mkdir -p ~/.config/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage ~/.local/bin/nvim
+
+
+# install lazyVim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
